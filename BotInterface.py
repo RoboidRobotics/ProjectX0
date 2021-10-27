8b3becc99516663709e2fc9542f8475353cc77c4
Roboid Robotics <92187728+RoboidRobotics@users.noreply.github.com>
2021-10-27 01:56:17 -0400

    Adding the .py file for the robot
    The BotInterface.py file contains a function that will give control of the servos from a different .py file

---
+++ BotInterface.py
@@ -0,0 +1,46 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+# Servo PWM pin numbers
+servo1 = 2 
+servo2 = 3
+
+# Setup the GPIO mode as BOARD so that you can reference the PINs on the board
+GPIO.setmode(GPIO.BOARD)
+
+# Set those pins to be voltage output
+GPIO.setup(servo1, GPIO.OUT)
+GPIO.setup(servo2, GPIO.OUT)
+
+# Setup the frequency of the PWM signals 
+# GPIO for PWM with 50Hz
+s1 = GPIO.PWM(servo1, 50)
+s2 = GPIO.PWM(servo2, 50) 
+
+# set the initial position/angle of the servos
+oldang1 = 0
+oldang2 = 0
+s1.start(oldang1) # Initialization
+s2.start(oldang2)
+
+
+def move(axis,angle): # Axis is "x","y","xy". Angle is from 0 - 180
+    angle = angle / 18 + 2 # https://www.instructables.com/Servo-Motor-Control-With-Raspberry-Pi/ <-(explains it best)
+    try:
+        if(axis == "x"):
+            s1.ChangeDutyCycle(angle) #  https://www.mbtechworks.com/projects/raspberry-pi-pwm.html
+            sleep(0.5) #sleep for half a microsecond
+        elif(axis == "y"):
+            s2.ChangeDutyCycle(angle)
+            sleep(0.5)
+        elif(axis == "xy"):
+            s1.ChangeDutyCycle(angle)  
+            s2.ChangeDutyCycle(angle)
+            sleep(0.5)
+
+    except KeyboardInterrupt:
+        s1.stop()
+        s2.stop()
+        GPIO.cleanup()
+
+
--- face_recognition.py
+++ face_recognition.py
@@ -14,11 +14,13 @@ __opencv_version__ = r'4.1.1'
 
 import sys
 import cv2 as cv
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+import BotInterface as bi
 
-print(cv.__file__)
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import matplotlib.image as mpimg
+
+
 if(__opencv_version__ != cv.__version__):
     print('WARNING: The OpenCV version being used ({}) is different from the OpenCV version this module was written in! ({})'.format(cv.__version__, __opencv_version__))
 
@@ -35,34 +37,50 @@ def detect_faces(image):
     image_gray = cv.cvtColor(image, cv.COLOR_RGB2GRAY) #create a grayscale copy of the image (used for face detection)
 
     #find the faces.
+    #TODO: Have an explanation as to how 'detectMultiScale' works so we can better-utilize scaleFactor and minNeighbors
+
     #** scaleFactor is a parameter specifying how much the image size is reduced at each image scale.
     #** minNeighbors is a parameter specifying how many neighbors each candidate rectangle should have to retain it.
-    faces_rects = __cascade__.detectMultiScale(image_gray, scaleFactor = 1.3, minNeighbors = 6)
+    faces_rects = __cascade__.detectMultiScale(image_gray, scaleFactor = 1.3, minNeighbors = 5)
 
     #print how many faces were found
     print('Faces found: {}'.format(len(faces_rects)))
+    return faces_rects
 
+
+
+def draw_rects(image, rects):
     #draw rectangles around the bounds of the detected faces
-    for (x,y,w,h) in faces_rects:
+    for (x,y,w,h) in rects:
         #draws a red rectangle with a thickness of 2
         cv.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-    return image
 
 
-# Using camera
-cap = cv.VideoCapture(0) # The Parameter is the index of the camera
-while 1:
-    ret, image = cap.read()  # Read each frame as an image
-    # test_image = cv.imread(image)
-    result_image = detect_faces(image)  # detect the faces
 
-    # #show the image and waits for a key press before exiting
-    cv.imshow('Detected Faces', result_image)
-    key = cv.waitKey(1) #waits for 2 milliseconds for a key press on a OpenCV window
-    if key == 113: # it breaks when q is pressed
-        break
-cap.release() 
-cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    # Using camera
+    cap = cv.VideoCapture(0) # The Parameter is the index of the camera
+    if not cap.isOpened():
+        print("Unable to capture camera")
+    while cap.isOpened():
+        ret, image = cap.read()  # Read each frame as an image
+        # test_image = cv.imread(image)
+        if ret:
+            face_rects = detect_faces(image)  # detect the faces
+            draw_rects(image, face_rects)
+
+            #TODO: change from an image display to a video display
+            # #show the image and waits for a key press before exiting
+            cv.imshow('Detected Faces', image)
+            key = cv.waitKey(1) #waits for 2 milliseconds for a key press on a OpenCV window
+            if key == 113: # it breaks when q is pressed
+                break
+        else:
+            print("Unable to get video frame from camera")
+            break
+    cap.release()
+    cv.destroyAllWindows()
 
 
 # FOR TESTEING (code is run only when called from command line)
@@ -76,10 +94,10 @@ cv.destroyAllWindows()
 #     cv.destroyAllWindows()
 
 # For testing from code
-# image = r'/Users/rediettadesse/Documents/GitHub/ProjectX0/image2.jpeg' #image full path
+# image = r'./Documents/GitHub/ProjectX0/image2.jpeg' #image full path
 # test_image =  cv.imread(image) #load the image from the file specified from the command line
 # result_image = detect_faces(test_image)  #detect the faces
-# #show the image and waits for a key press before exiting
+#show the image and waits for a key press before exiting
 
 # cv.imshow('Detected Faces', result_image)
 # # print(cv.waitKey(0))
